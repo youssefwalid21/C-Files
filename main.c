ec8222885c52c5fc04bd15fbfea795ecb8b30877
youssefwalid21 <86124686+youssefwalid21@users.noreply.github.com>
2023-08-31 17:18:57 +0300

    main.c

--- main.c
+++ main.c
@@ -18,37 +18,39 @@ int main()
 
     do
     {
-        logo(); //include for the logo
+        logo(); //Include for the logo
         printf("============================================================\n");
         printf("==== Pick Sorting Order (1: Ascending - 2: Descending): ====\n");
         printf("============================================================\n");
         printf(">> ");
-        scanf("%d", &choice); //scaning
+        scanf("%d", &choice); // Scaning the choice for the switch case
         fflush(stdin);
 
         switch (choice)
         {
         case 1:
-            input(names);
-            Sort_Ascending(names);
+            input(names); // The Input Function to scan the seven arrays
+            Sort_Ascending(names); // This function to sort the seven names from A ot Z
             break;
 
         case 2:
-            input(names);
-            Sort_Descending(names);
+            input(names); // The Input Function to scan the seven arrays
+            Sort_Descending(names); // This function to sort the seven names from Z ot A
             break;
 
-        default:
-            printf("Error!!\n");
-            logo();
+        default: // In case the user didn't enter 1 or 2 
+            printf("Invlaid Choice!!\n");
+            
+            break;
+        }
+        // logo();
             printf("============================================================\n");
             printf("============== Do You Want Try Again ( -> Y - -> N): =======\n");
             printf("============================================================\n");
             printf(":\n>> ");
             scanf("%c", &choice_do);
             fflush(stdin);
-            break;
-        }
+        
     } while (choice_do == 'y' || choice_do == 'Y');
 
     return 0;
